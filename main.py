5988093f83101be08bd671e256920d8b7e7ef23a
martroben <mart.roben@gmail.com>
2023-04-23 22:56:33 +0300

    Finished naive accident data graph.

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ import io
 import json
 # external
 import pandas as pd
-# import plotly.graph_objects as go
-# from plotly.subplots import make_subplots
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
 # local
 import api_interface
 import data_operations
@@ -127,58 +127,116 @@ harm_by_day = traffic_accidents\
         n_harmed_bicycle_cumulative=lambda df: df["n_harmed_bicycle"].cumsum())
 
 
-############# fix column names
+#############################
+# Naive accident data graph #
+#############################
+
+motor_vehicle_color = "#526a83"
+bicycle_color = "#a06177"
+daily_yaxis_max = 1.1 * max([max(harm_by_day["n_harmed_bicycle"]),
+                             max(harm_by_day["n_harmed_motor_vehicle"])])
+cumulative_yaxis_max = 1.1 * max([max(harm_by_day["n_harmed_bicycle_cumulative"]),
+                                  max(harm_by_day["n_harmed_motor_vehicle_cumulative"])])
+xaxis_min = min(harm_by_day["day"])
+xaxis_max = max(harm_by_day["day"])
+motor_vehicle_bicycle_ratio = max(harm_by_day["n_harmed_motor_vehicle_cumulative"]) /\
+                              max(harm_by_day["n_harmed_bicycle_cumulative"])
 
 figure = make_subplots(
-    rows=3, cols=1, row_heights=[40, 2, 2],
+    rows=3, cols=1,
+    row_heights=[40, 5, 5],
     shared_xaxes=True,
     vertical_spacing=0.05)
 
-motor_vehicle_total_graph = go.Bar(
-    name="people hurt in motor vehicle accidents",
+motor_vehicle_by_day_graph = go.Bar(
+    name="people hurt in motor vehicle accidents per day",
     x=harm_by_day["day"],
-    y=harm_by_day["harmed_vehicle"],
-    marker={"color": "#526a83", "line_color": "#526a83"})
+    y=harm_by_day["n_harmed_motor_vehicle"],
+    marker=dict(
+        color=motor_vehicle_color,
+        line_color=motor_vehicle_color),
+    showlegend=False)
 
-bicycle_total_graph = go.Bar(
-    name="people hurt in bicycle accidents",
+bicycle_by_day_graph = go.Bar(
+    name="people hurt in bicycle accidents per day",
     x=harm_by_day["day"],
-    y=harm_by_day["harmed_bicycle"],
-    marker={"color": "#a06177", "line_color": "#a06177"})
+    y=harm_by_day["n_harmed_bicycle"],
+    marker=dict(
+        color=bicycle_color,
+        line_color=bicycle_color),
+    showlegend=False)
 
 motor_vehicle_cumulative_graph = go.Scatter(
+    name="deaths + injuries in <b>motor vehicle</b> accidents",
     x=harm_by_day["day"],
-    y=harm_by_day["harmed_vehicle_cumulative"],
-    marker={"color": "#526a83", "line_color": "#526a83"},
-    fill="tozeroy",
-    showlegend=False)
+    y=harm_by_day["n_harmed_motor_vehicle_cumulative"],
+    line=dict(
+        color=motor_vehicle_color,
+        width=1),
+    fill="tozeroy")
 
 bicycle_cumulative_graph = go.Scatter(
+    name="deaths + injuries in <b>bicycle</b> accidents",
     x=harm_by_day["day"],
-    y=harm_by_day["harmed_bicycle_cumulative"],
-    marker={"color": "#a06177", "line_color": "#a06177"},
-    fill="tozeroy",
-    showlegend=False)
+    y=harm_by_day["n_harmed_bicycle_cumulative"],
+    line=dict(
+        color=bicycle_color,
+        width=1),
+    fill="tozeroy")
+
+motor_vehicle_cumulative_highest = go.Scatter(
+    x=[max(harm_by_day["day"])],
+    y=[max(harm_by_day["n_harmed_motor_vehicle_cumulative"])],
+    mode="markers+text",
+    marker=dict(
+        color=motor_vehicle_color,
+        size=8),
+    text=[int(max(harm_by_day["n_harmed_motor_vehicle_cumulative"]))],
+    textposition="middle right",
+    showlegend=False,
+    cliponaxis=False)
+
+bicycle_cumulative_highest = go.Scatter(
+    x=[max(harm_by_day["day"])],
+    y=[max(harm_by_day["n_harmed_bicycle_cumulative"])],
+    mode="markers+text",
+    marker=dict(
+        color=bicycle_color,
+        size=8),
+    text=[int(max(harm_by_day["n_harmed_bicycle_cumulative"]))],
+    textposition="middle right",
+    showlegend=False,
+    cliponaxis=False)
 
 
 figure.add_trace(motor_vehicle_cumulative_graph, row=1, col=1)
 figure.add_trace(bicycle_cumulative_graph, row=1, col=1)
-figure.add_trace(motor_vehicle_total_graph, row=2, col=1)
-figure.add_trace(bicycle_total_graph, row=3, col=1)
+figure.add_trace(motor_vehicle_cumulative_highest, row=1, col=1)
+figure.add_trace(motor_vehicle_by_day_graph, row=2, col=1)
+figure.add_trace(bicycle_by_day_graph, row=3, col=1)
+figure.add_trace(bicycle_cumulative_highest, row=1, col=1)
+
 figure.update_layout(
     bargap=0,
-    # autosize=False,
+    autosize=False,
     # width=1000,
-    # height=500,
+    height=800,
     plot_bgcolor="white",
-    yaxis1_range=[0, 2e+4],
-    yaxis2_range=[0, 30],
-    yaxis3_range=[0, 30],
+    yaxis1_range=[0, cumulative_yaxis_max],
+    yaxis2_range=[0, daily_yaxis_max],
+    yaxis3_range=[0, daily_yaxis_max],
+    xaxis1_range=[xaxis_min, xaxis_max],
     yaxis1_tickfont_size=10,
     yaxis2_tickfont_size=8,
-    yaxis3_tickfont_size=8)
-
+    yaxis3_tickfont_size=8,
+    legend=dict(
+        yanchor="top",
+        y=1.05,
+        xanchor="left",
+        x=0.01,
+        bgcolor="rgba(0,0,0,0)"))
 figure.update_yaxes(gridcolor="lightgrey")
+
 figure.show()
 
 
